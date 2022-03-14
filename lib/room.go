2f3cba77bc0cece74a665f0786deac43e3ed1f6a
Louis Dunnill <43004304+dunnill@users.noreply.github.com>
2022-03-14 08:24:29 +0000

    [LessRepeat] Corrections post conflict resolve

--- lib/room.go
+++ lib/room.go
@@ -3,16 +3,17 @@ package lib
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 
 	"github.com/alicekaerast/ioffice/schema"
 )
 
-func (i *IOffice) GetRoom(ID string) schema.Room {
-	endpoint := fmt.Sprintf("v2/rooms/%v", ID)
+func (i *IOffice) GetRoom(search string) schema.Room {
+	endpoint := fmt.Sprintf("v2/rooms/%v", search)
 	body := i.Request("GET", endpoint, nil)
 	rooms := make([]schema.Room, 0)
 	json.Unmarshal([]byte(body), &rooms)
-  if len(rooms) == 0 {
+	if len(rooms) == 0 {
 		log.Fatalf("Couldn't find any rooms for search %v", search)
 	}
 	return rooms[0]
