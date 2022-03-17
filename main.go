fea86d773af765710f0564cf315b48b28f88c084
Oliver Leaver-Smith <oliver@leaversmith.com>
2022-03-17 16:09:40 +0000

    Check argument count before making any connections

--- main.go
+++ main.go
@@ -15,35 +15,35 @@ func usage() {
 }
 
 func main() {
-	viper.SetConfigName("ioffice")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("$HOME")
-	viper.AddConfigPath("$HOME/.config")
-	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
-	if err != nil { // Handle errors reading the config file
-		panic(fmt.Errorf("Fatal error config file: %w \n", err))
-	}
-	viper.SetEnvPrefix("ioffice")
-	viper.AutomaticEnv()
-
-	username := viper.GetString("username")
-	password := viper.GetString("password")
-	roomID := viper.GetInt("roomID")
-	hostname := viper.GetString("hostname")
-	session := viper.GetString("session")
-
-	ioffice := lib.NewIOffice(hostname, username, password, session)
-
-	me := ioffice.GetMe()
-	if !ioffice.WasOkay() {
-		log.Println("Stopping now as auth failed.  Are you on SSO?  See README.md on how to authenticate.")
-		return
-	}
-
 	if len(os.Args) < 2 {
 		usage()
 	} else {
+		viper.SetConfigName("ioffice")
+		viper.SetConfigType("yaml")
+		viper.AddConfigPath("$HOME")
+		viper.AddConfigPath("$HOME/.config")
+		viper.AddConfigPath(".")
+		err := viper.ReadInConfig()
+		if err != nil { // Handle errors reading the config file
+			panic(fmt.Errorf("Fatal error config file: %w \n", err))
+		}
+		viper.SetEnvPrefix("ioffice")
+		viper.AutomaticEnv()
+
+		username := viper.GetString("username")
+		password := viper.GetString("password")
+		roomID := viper.GetInt("roomID")
+		hostname := viper.GetString("hostname")
+		session := viper.GetString("session")
+
+		ioffice := lib.NewIOffice(hostname, username, password, session)
+
+		me := ioffice.GetMe()
+		if !ioffice.WasOkay() {
+			log.Println("Stopping now as auth failed.  Are you on SSO?  See README.md on how to authenticate.")
+			return
+		}
+
 
 		switch os.Args[1] {
 		case "list":
