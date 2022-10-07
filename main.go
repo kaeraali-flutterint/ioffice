60d45143dec9b5fea0f4031c6c5a3e63aeac3511
alicekaerast <alice_kaerast@flutterint.com>
2022-10-07 12:54:48 +0100

    chore: simplify main() function

--- main.go
+++ main.go
@@ -14,6 +14,116 @@ import (
 	"github.com/spf13/viper"
 )
 
+func main() {
+	configure()
+	buildingID := viper.GetInt("buildingID")
+	roomID := viper.GetInt("roomID")
+
+	ioffice, me := auth()
+
+	if len(os.Args) < 2 {
+		ioffice.ListReservations()
+		return
+	}
+
+	switch os.Args[1] {
+	case "list":
+		ioffice.ListReservations()
+	case "create":
+		createReservation(ioffice, me, roomID, buildingID)
+	case "checkin":
+		checkin(ioffice)
+	case "cancel":
+		cancelCheckin(ioffice)
+	case "buildings":
+		listBuildings(ioffice)
+	case "occupancy":
+		listOccupancy(ioffice)
+	case "floors":
+		tbl := listFloors(buildingID, ioffice)
+		tbl.Print()
+	default:
+		usage()
+	}
+}
+
+func listFloors(buildingID int, ioffice *lib.IOffice) table.Table {
+	if len(os.Args) == 3 {
+		buildingID, _ = strconv.Atoi(os.Args[2])
+	}
+	floors := make([]schema.Floor, 0)
+	if buildingID == 0 {
+		floors = ioffice.Floors()
+	} else {
+		floors = ioffice.FloorsForBuilding(fmt.Sprint(buildingID))
+	}
+
+	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
+	columnFmt := color.New(color.FgYellow).SprintfFunc()
+	tbl := table.New("ID", "Name")
+	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+
+	for _, floor := range floors {
+		tbl.AddRow(floor.ID, floor.Name)
+	}
+	return tbl
+}
+
+func listOccupancy(ioffice *lib.IOffice) {
+	if len(os.Args) != 3 {
+		fmt.Print("Must provide a floor ID for occupancy")
+		fmt.Print("Usage: ioffice occupancy <floor ID>")
+		os.Exit(1)
+	}
+	floorID, _ := strconv.Atoi(os.Args[2])
+	ioffice.ShowOccupancy(floorID)
+}
+
+func listBuildings(ioffice *lib.IOffice) {
+	buildings := ioffice.Buildings()
+
+	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
+	columnFmt := color.New(color.FgYellow).SprintfFunc()
+	tbl := table.New("ID", "Name")
+	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+
+	for _, building := range buildings {
+		tbl.AddRow(building.ID, building.Name)
+	}
+	tbl.Print()
+}
+
+func cancelCheckin(ioffice *lib.IOffice) {
+	reservationID := os.Args[2]
+	ioffice.CancelReservation(reservationID)
+	ioffice.ListReservations()
+}
+
+func checkin(ioffice *lib.IOffice) {
+	reservationID := os.Args[2]
+	ioffice.CheckIn(reservationID)
+	ioffice.ListReservations()
+}
+
+func createReservation(ioffice *lib.IOffice, me schema.User, roomID int, buildingID int) {
+	if len(os.Args) == 2 {
+		usage()
+	}
+	if len(os.Args) == 3 {
+		ioffice.CreateReservation(me, roomID, dateparse.MustParse(os.Args[2]))
+	}
+	if len(os.Args) == 4 {
+		room := schema.Room{}
+		if buildingID == 0 {
+			room = ioffice.GetRoom(os.Args[3])
+		} else {
+			room = ioffice.GetRoomWithBuilding(os.Args[3], buildingID)
+		}
+		ioffice.CreateReservation(me, room.ID, dateparse.MustParse(os.Args[2]))
+	}
+	ioffice.ListReservations()
+}
+
 func usage() {
 	fmt.Printf(`Please use one of the following commands:
 
@@ -21,10 +131,11 @@ func usage() {
 %v create <yyyy-mm-dd> [room name]
 %v checkin <reservation ID>
 %v cancel <reservation ID>
-%v buildings`, os.Args[0], os.Args[0], os.Args[0], os.Args[0], os.Args[0])
+%v buildings
+%v floors`, os.Args[0], os.Args[0], os.Args[0], os.Args[0], os.Args[0], os.Args[0])
 }
 
-func main() {
+func configure() {
 	viper.SetDefault("buildingID", 0)
 	viper.SetConfigName("ioffice")
 	viper.SetConfigType("yaml")
@@ -37,97 +148,13 @@ func main() {
 	}
 	viper.SetEnvPrefix("ioffice")
 	viper.AutomaticEnv()
+}
 
-	username := viper.GetString("username")
-	password := viper.GetString("password")
-	buildingID := viper.GetInt("buildingID")
-	roomID := viper.GetInt("roomID")
-	hostname := viper.GetString("hostname")
-	session := viper.GetString("session")
-
-	ioffice := lib.NewIOffice(hostname, username, password, session)
-
+func auth() (*lib.IOffice, schema.User) {
+	ioffice := lib.NewIOffice(viper.GetString("hostname"), viper.GetString("username"), viper.GetString("password"), viper.GetString("session"))
 	me := ioffice.GetMe()
 	if !ioffice.WasOkay() {
-		log.Println("Stopping now as auth failed.  Are you on SSO?  See README.md on how to authenticate.")
-		return
+		log.Fatalln("Stopping now as auth failed.  Are you on SSO?  See README.md on how to authenticate.")
 	}
-
-	if len(os.Args) < 2 {
-		ioffice.ListReservations()
-	} else {
-
-		switch os.Args[1] {
-		case "list":
-			ioffice.ListReservations()
-		case "create":
-			if len(os.Args) == 2 {
-				usage()
-			}
-			if len(os.Args) == 3 {
-				ioffice.CreateReservation(me, roomID, dateparse.MustParse(os.Args[2]))
-			}
-			if len(os.Args) == 4 {
-				room := schema.Room{}
-				if buildingID == 0 {
-					room = ioffice.GetRoom(os.Args[3])
-				} else {
-					room = ioffice.GetRoomWithBuilding(os.Args[3], buildingID)
-				}
-				ioffice.CreateReservation(me, room.ID, dateparse.MustParse(os.Args[2]))
-			}
-			ioffice.ListReservations()
-		case "checkin":
-			reservationID := os.Args[2]
-			ioffice.CheckIn(reservationID)
-			ioffice.ListReservations()
-		case "cancel":
-			reservationID := os.Args[2]
-			ioffice.CancelReservation(reservationID)
-			ioffice.ListReservations()
-		case "buildings":
-			buildings := ioffice.Buildings()
-
-			headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
-			columnFmt := color.New(color.FgYellow).SprintfFunc()
-			tbl := table.New("ID", "Name")
-			tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
-
-			for _, building := range buildings {
-				tbl.AddRow(building.ID, building.Name)
-			}
-			tbl.Print()
-		case "occupancy":
-			if len(os.Args) != 3 {
-				fmt.Print("Must provide a floor ID for occupancy")
-				fmt.Print("Usage: ioffice occupancy <floor ID>")
-				os.Exit(1)
-			}
-			floorID, _ := strconv.Atoi(os.Args[2])
-			ioffice.ShowOccupancy(floorID)
-		case "floors":
-			if len(os.Args) == 3 {
-				buildingID, _ = strconv.Atoi(os.Args[2])
-			}
-			floors := make([]schema.Floor, 0)
-			if buildingID == 0 {
-				floors = ioffice.Floors()
-			} else {
-				floors = ioffice.FloorsForBuilding(fmt.Sprint(buildingID))
-			}
-
-			headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
-			columnFmt := color.New(color.FgYellow).SprintfFunc()
-			tbl := table.New("ID", "Name")
-			tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
-
-			for _, floor := range floors {
-				tbl.AddRow(floor.ID, floor.Name)
-			}
-			tbl.Print()
-
-		default:
-			usage()
-		}
-	}
+	return ioffice, me
 }
